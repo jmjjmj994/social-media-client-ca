9816501e8d2dedb80d9f6951ad9a7deb16a5ffa0
jmjjmj994 <jonathan.mjos@outlook.com>
2023-11-13 12:16:43 +0100

    Final push before delivery.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -10,8 +10,6 @@ describe("login", () => {
         cy.get("#registerForm > div.modal-footer > button.btn.btn-outline-success").click().wait(1000);
         cy.get('#loginEmail').type(`${email}`);
         cy.get("#loginPassword").type(`${password}`)
-
-
         cy.get(".btn-success").contains("Login").click()
     })
 })
